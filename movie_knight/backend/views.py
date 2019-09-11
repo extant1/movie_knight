856def77d79c79495ae335562d08f68ddb8833a8
Whiskey Maple <ext.whiskey@gmail.com>
2019-09-10 23:53:12 -0400

    -return stream url

--- movie_knight/backend/views.py
+++ movie_knight/backend/views.py
@@ -33,7 +33,7 @@ def on_publish():
         abort(403)
     else:
         stream.live_now()
-        return stream.user.get_name
+        return url_for('public.stream', username=stream.user.get_name)
     abort(403)
 
 
