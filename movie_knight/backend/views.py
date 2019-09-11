4a623b2abbe7c4c61bdd8bce5639c7d0f673803b
Whiskey Maple <ext.whiskey@gmail.com>
2019-09-11 00:20:36 -0400

    -reeeee

--- movie_knight/backend/views.py
+++ movie_knight/backend/views.py
@@ -33,7 +33,7 @@ def on_publish():
         abort(403)
     else:
         stream.live_now()
-        return redirect(url_for('public.stream', username=stream.user.get_name))
+        return redirect('https://m.theuberserver.com/{username}'.format(username=stream.user.get_name))
     abort(403)
 
 
