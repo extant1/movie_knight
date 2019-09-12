17bfee3da363382ac11d7a5d5b6489c6a5b99858
Whiskey Maple <ext.whiskey@gmail.com>
2019-09-11 21:05:05 -0400

    reeee?

--- movie_knight/backend/views.py
+++ movie_knight/backend/views.py
@@ -35,7 +35,7 @@ def on_publish():
     else:
         stream.live_now()
         # return stream.user.get_name
-        return movie_redirect(stream.user.get_name, 302)
+        return redirect("rtmp://127.0.0.1/hls-live/" + stream.user.get_name, 302)
     abort(403)
 
 
